60b0d5312a2fd7563665c2b89a4b818c01b21935
Patrick Clay <Patrick@localhost.(none)>
2011-05-03 01:19:03 -0400

    fixed merge

---
+++ Scheduler/src/backbone/MemberAttribute.java
@@ -0,0 +1,16 @@
+package backbone;
+
+public class MemberAttribute extends Attribute {
+
+	public MemberAttribute(String title) {
+		super(title);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public Type getType() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
