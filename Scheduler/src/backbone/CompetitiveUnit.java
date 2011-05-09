52d1d179a923a6db6d327e0ad198e390bd5f764b
Matt Mahoney <mjmahone@cs.brown.edu>
2011-05-09 15:02:12 -0400

    eh

--- Scheduler/src/backbone/CompetitiveUnit.java
+++ Scheduler/src/backbone/CompetitiveUnit.java
@@ -2,6 +2,12 @@ package backbone;
 
 import java.util.ArrayList;
 
-
+/**
+ * The interface used for describing a competitive unit
+ * 
+ * @deprecated
+ * @author pclay
+ *
+ */
 public interface CompetitiveUnit extends Unit {
 }
